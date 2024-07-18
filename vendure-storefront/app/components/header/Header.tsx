ac24d53f42d22cef32ed6cc00b83924e5fcf55e2
Julio Miranda B <96454553+JulioMirandaB@users.noreply.github.com>
2024-07-18 12:49:20 -0500

    dropdown products

--- vendure-storefront/app/components/header/Header.tsx
+++ vendure-storefront/app/components/header/Header.tsx
@@ -1,11 +1,12 @@
 import { Link, useLoaderData } from '@remix-run/react';
-import { ShoppingBagIcon } from '@heroicons/react/24/outline';
+import { ShoppingBagIcon, ChevronDownIcon } from '@heroicons/react/24/outline';
 import { SearchBar } from '~/components/header/SearchBar';
 import { useRootLoader } from '~/utils/use-root-loader';
 import { UserIcon } from '@heroicons/react/24/solid';
 import { useScrollingUp } from '~/utils/use-scrolling-up';
 import { classNames } from '~/utils/class-names';
 import { useTranslation } from 'react-i18next';
+import { useState } from 'react';
 
 export function Header({
   onCartIconClick,
@@ -18,41 +19,17 @@ export function Header({
   const isSignedIn = !!data.activeCustomer.activeCustomer?.id;
   const isScrollingUp = useScrollingUp();
   const { t } = useTranslation();
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+
+  const toggleDropdown = () => setDropdownOpen(!dropdownOpen);
 
   return (
     <header
       className={classNames(
-        isScrollingUp ? 'sticky top-0 z-10 animate-dropIn' : '',
-        'bg-gradient-to-r from-zinc-700 to-gray-900 shadow-lg transform shadow-xl',
+        isScrollingUp ? 'sticky top-0 z-50 animate-dropIn' : '',
+        'bg-gradient-to-r from-zinc-700 to-gray-900 shadow-lg  shadow-xl',
       )}
     >
-      <div className="bg-zinc-100 text-gray-600 shadow-inner text-center text-sm py-2 px-2 xl:px-0">
-        <div className="max-w-6xl mx-2 md:mx-auto flex items-center justify-between">
-          <div>
-            <p className="hidden sm:block">
-              {t('vendure.exclusive')}{' '}
-              <a
-                href="https://funkyton.com/vendure-tutorial/"
-                target="_blank"
-                className="underline"
-              >
-                {t('vendure.repoLinkLabel')}
-              </a>
-            </p>
-          </div>
-          <div>
-            <Link
-              to={isSignedIn ? '/account' : '/sign-in'}
-              className="flex space-x-1"
-            >
-              <UserIcon className="w-4 h-4"></UserIcon>
-              <span>
-                {isSignedIn ? t('account.myAccount') : t('account.signIn')}
-              </span>
-            </Link>
-          </div>
-        </div>
-      </div>
       <div className="max-w-6xl mx-auto p-4 flex items-center space-x-4">
         <h1 className="text-white w-30">
           <Link to="/">
@@ -60,24 +37,39 @@ export function Header({
               src="https://aeiom.com/isw/Logo-ISW-header.png"
               width={100}
               height={64}
-              alt={t('commmon.logoAlt')}
+              alt={t('common.logoAlt')}
             />
           </Link>
         </h1>
         <div className="flex space-x-4 hidden sm:block">
-          {data.collections.map((collection) => (
-            <Link
-              className="text-sm md:text-base text-gray-200 hover:text-white"
-              to={'/collections/' + collection.slug}
-              prefetch="intent"
-              key={collection.id}
+          <div className="relative">
+            <button
+              onClick={toggleDropdown}
+              className="flex items-center space-x-1 bg-gray-800 text-gray-200 hover:text-white p-2 rounded"
             >
-              {collection.name}
-            </Link>
-          ))}
+              <span>Products</span>
+              <ChevronDownIcon className="w-5 h-5" />
+            </button>
+            {dropdownOpen && (
+              <ul className="absolute z-20 bg-gray-800 text-white shadow-lg rounded mt-2">
+                {data.collections.map((collection) => (
+                  <h1><li key={collection.id} className="border-b-2 border-blue-900 hover:bg-gray-700">
+                    <Link
+                      to={'/collections/' + collection.slug}
+                      className="block text-sm p-2"
+                      onClick={() => setDropdownOpen(false)}
+                      prefetch="intent"
+                    >
+                      {collection.name}
+                    </Link>
+                  </li></h1>
+                ))}
+              </ul>
+            )}
+          </div>
         </div>
         <div className="flex-1 md:pr-8">
-          <SearchBar></SearchBar>
+          <SearchBar />
         </div>
         <div className="">
           <button
@@ -85,7 +77,7 @@ export function Header({
             onClick={onCartIconClick}
             aria-label="Open cart tray"
           >
-            <ShoppingBagIcon></ShoppingBagIcon>
+            <ShoppingBagIcon />
             {cartQuantity ? (
               <div className="absolute rounded-full -top-2 -right-2 bg-primary-600 min-w-6 min-h-6 flex items-center justify-center text-xs p-1">
                 {cartQuantity}
